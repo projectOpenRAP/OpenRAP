7c436accbf02fa093f6f17ba0f3d297a63722518
Skeletrox <android.svr@gmail.com>
2017-11-11 18:30:55 +0530

    fix bugs #26, #31 and #32

--- devmgmt/file_upload/changecaptive/urls.py
+++ devmgmt/file_upload/changecaptive/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	url (r'^change_data/$', views.change_data, name='changedata'),
 	url (r'^change_logo/$', views.change_logo, name='changelogo'),
 	url (r'^change_apk/$', views.change_apk, name='changeapk'),
-	url (r'^captive_display/$', views.captive_display, name='captivedisplay')
+	url (r'^captive_display/$', views.captive_display, name='captivedisplay'),
 	url (r'^return_text/$', views.return_text, name="returntext")
 ]
--- devmgmt/file_upload/fileupload/static/js/scripts.js
+++ devmgmt/file_upload/fileupload/static/js/scripts.js
@@ -1,14 +1,14 @@
 // Event handling
 /*document.addEventListener("DOMContentLoaded",
   function (event) {
-    
+
     // Unobtrusive event binding
     document.querySelector("#usb")
       .addEventListener("click", function () {
-        
+
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("http://127.0.0.1:8000/upload/transfer/", 
+          .sendGetRequest("http://127.0.0.1:8000/upload/transfer/",
             function (request) {
               //var name = request.responseText;
             console.log(request.responseText);
@@ -17,15 +17,15 @@
                 //.innerHTML = "<h2>Hello " + name + "!</h2>";
             });
 
-        
+
       });
   }
 );
-*/
+
 
 
 document.querySelector("#usb")
       .addEventListener("click", function () {
-        
+
         window.open("/upload/transfer/", "_self");
-});
+});*/
