fb54d1b8cc464d16a7a6b9fdfc7e21ce131ba514
Rishabh Jain <mailrishabh1403@gmail.com>
2018-01-09 09:59:31 +0530

    structure for backend

---
+++ devmgmtV2/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let init = (req,res) => {
+    res.send("Hello init")
+}
+
+module.exports = {
+    init
+}
---
+++ devmgmtV2/index.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let express = require("express");
+let bodyParser = require('body-parser');
+let app = express();
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+require('./routes/auth.routes.js')(app);
+
+app.listen(8080, err => {
+    if (err)
+        console.log(err)
+    else
+        console.log("server running on port 8080")
+});
