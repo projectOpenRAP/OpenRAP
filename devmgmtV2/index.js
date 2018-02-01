21493fff634e3996e21805644f7b12d9d8012e10
Skeletrox <android.svr@gmail.com>
2018-02-01 11:53:07 +0530

    complete dm-28 [add init script for sql to index.js]

--- devmgmtV2/index.js
+++ devmgmtV2/index.js
@@ -4,6 +4,7 @@ let express = require("express");
 let bodyParser = require('body-parser');
 let cors = require('cors');
 let app = express();
+let { exec } = require('child_process')
 
 app.use(cors())
 // parse application/x-www-form-urlencoded
@@ -20,6 +21,11 @@ require('./routes/filemgmt.routes.js')(app);
 require('./routes/ssid.routes.js')(app);
 require('./routes/captive.routes.js')(app);
 
+exec('mysql -u root -p < ./init.sql', (err, stdout, stderr) => {
+  console.log("System errors: " + err);
+  console.log("System output: " + stdout);
+  console.log("Script errors: " + stderr);
+});
 app.listen(8080, err => {
     if (err)
         console.log(err);
