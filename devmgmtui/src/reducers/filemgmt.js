fa655634a33e327470ae1010dc8ae804015950ae
Skeletrox <android.svr@gmail.com>
2018-02-09 11:52:32 +0530

    pre merge commit

--- devmgmtui/src/reducers/filemgmt.js
+++ devmgmtui/src/reducers/filemgmt.js
@@ -1,4 +1,4 @@
-export default function (state={currentDir : '/var/www/', files : [], uploadableFiles : [], selectedFiles : [], allSelected : false}, action){
+export default function (state={currentDir : '/var/www/', files : [], uploadableFiles : [], selectedFiles : [], allSelected : false, usbDir : ''}, action){
     switch(action.type){
       case 'OPEN_DIR' :
         return {...state, currentDir : action.payload}
@@ -8,6 +8,8 @@ export default function (state={currentDir : '/var/www/', files : [], uploadable
         return {...state, uploadableFiles : action.payload}
       case 'SELECT_FILES' :
         return {...state, selectedFiles : action.payload}
+      case 'USB_DIR' :
+        return {...state, usbDir : action.payload}
       default:
         return state
     }
