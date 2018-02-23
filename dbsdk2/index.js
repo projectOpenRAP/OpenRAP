ae2d12f21fae953c69b95914bae7ed9ca047ffd5
rak16 <ravishahmad16@gmail.com>
2018-02-23 11:51:58 +0530

    Add delete database function

--- dbsdk2/index.js
+++ dbsdk2/index.js
@@ -45,3 +45,24 @@ let listDatabases  = () => {
 
     return defer.promise;
 }
+
+let deleteDatabase = (params) => {
+    let defer = q.defer();
+
+    let connection = mysql.createConnection(getDbCredentials());
+    connection.connect();
+
+    let query = mysql.format('DROP DATABASE ??', [params.db.name]);
+
+    connection.query(query, (error, results, fields) => {
+        if(error) {
+            defer.reject(error);
+        } else {
+            defer.resolve(results); // TODO Change this to an apt message
+        }
+    });
+
+    connection.end();
+
+    return defer.promise;
+}
