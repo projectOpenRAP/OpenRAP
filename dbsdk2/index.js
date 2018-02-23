6cef84bcba9aee4d1af38372b020bf0bf9013a2f
rak16 <ravishahmad16@gmail.com>
2018-02-23 11:51:19 +0530

    Add create database function

--- dbsdk2/index.js
+++ dbsdk2/index.js
@@ -3,3 +3,23 @@
 const mysql                 =   require('mysql');
 const q                     =   require('q');
 const { getDbCredentials }  =   require('./config');
+
+let createDatabase = (params) => {
+    let defer = q.defer();
+
+    let connection = mysql.createConnection(getDbCredentials());
+
+    let query = mysql.format('CREATE DATABASE IF NOT EXISTS ??', [params.db.name]);
+
+    connection.query(query, (error, results, fields) => {
+        if(error) {
+            defer.reject(error);
+        } else {
+            defer.resolve(results); // TODO Change this to an apt message
+        }
+    });
+
+    connection.end();
+
+    return defer.promise;
+}
