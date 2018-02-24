69c5515ce162952fa81d1e5f84d9ec24cddd15bb
Skeletrox <android.svr@gmail.com>
2018-02-24 15:43:48 +0530

    add config, fix responses and bugs

---
+++ searchsdk/config.js
@@ -0,0 +1,11 @@
+const SEARCH_SERVER = `http://0.0.0.0:9095`;
+const INIT_BASE_URL = `api/search/v2/edu/init`;
+const INDEX_BASE_URL = `api/search/v2/index`;
+const DOCUMENT_URL_EXT = `document`;
+
+module.exports = {
+    SEARCH_SERVER,
+    INIT_BASE_URL,
+    INDEX_BASE_URL,
+    DOCUMENT_URL_EXT
+}
--- searchsdk/index.js
+++ searchsdk/index.js
@@ -3,8 +3,7 @@
 let request = require('request');
 let q = require('q');
 let fs = require('fs');
-const SEARCH_SERVER='http://0.0.0.0:9095';
-
+let { SEARCH_SERVER, INIT_BASE_URL, INDEX_BASE_URL, DOCUMENT_URL_EXT } = require('./config.js');
 
 /*
     Uses params object with the following structure:
@@ -14,32 +13,12 @@ const SEARCH_SERVER='http://0.0.0.0:9095';
         documentID : name [id] of the document to be queried [not needed for addDocument] [only needed for operations involving documents],
         searchString : String to Search [only used in search]
     }
-
     Returns a promise with either of the following:
-    On successful execution of the query [except in document retrieval]: Resolve
+    On successful execution of the query
     {
-        body : {
-            status : "ok" [If the execution is successful],
-            count : [The number returned by search and count] [Only returned by count and search]
-        },
-        status : HTTP Status Code of the response
+        body : Body of the response,
+        success : true
     }
-
-    For document retrieval: Resolve
-    {
-        body : {
-            id : doocumentID,
-            fields : fields within the document in JSON format
-        },
-        status : HTTP Status Code of the response
-    }
-
-    If the query is legitimate but cannot be satisfied [due to missing data, eg. querying a document that does not exist]: Resolve
-    {
-        body : The message returned by the server,
-        status : HTTP status code of the response
-    }
-
     If there is an error: Reject
     {
         err : The error string,
@@ -52,246 +31,292 @@ const SEARCH_SERVER='http://0.0.0.0:9095';
 let init = () => {
     let defer = q.defer();
     let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/edu/init`,
+        url : `${SEARCH_SERVER}/${INIT_BASE_URL}`,
         method : 'PUT'
     }
     request(options, (err, res, body) => {
         if (err) {
             return defer.reject({err, at : "init"});
         } else {
-            return defer.resolve({body, status : res.statusCode})
+            return defer.resolve({body, success : true})
         }
     });
     return defer.promise;
 }
 
 //Create a new index [PUT] Creates a new Index
-let createIndex = (params, cb) => {
+let createIndex = (params) => {
     let defer = q.defer();
     let indexName = params.indexName;
     let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName,
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}`,
         method : 'PUT'
     }
     request(options, (err, res, body) => {
         if (err) {
             return defer.reject({err, at : "Create Index"});
         } else {
-            return defer.resolve({body, status : res.statusCode});
-        }
-    });
-    return defer.promise;
-}
-
-//Delete an index [DELETE] Deletes an Index
-let deleteIndex = (params, cb) => {
-    let defer = q.defer();
-    let indexName = params.indexName;
-    let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName,
-        method : 'DELETE'
-    }
-    request(options, (err, res, body) => {
-        if (err) {
-            return defer.reject({err, at : "Delete Index"});
-        } else {
-            return defer.resolve({body, status : res.statusCode});
-        }
-    });
-    return defer.promise;
-}
-
-//Get all indices [GET] Gets all Indices
-let getAllIndices = (cb) => {
-    let defer = q.defer();
-    let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/`,
-        method : 'GET'
-    }
-    request(options, (err, res, body) => {
-        if (err) {
-            return defer.reject({err, at : "Get Indices"});
-        } else {
-            return defer.resolve({body, status : res.statusCode});
-        }
-    });
-    return defer.promise;
-}
-
-//Add a document [PUT] Adds a document to an index
-let addDocument = (params, cb) => {
-    let defer = q.defer();
-    let indexName = params.indexName;
-    let documentPath = params.documentPath;
-    let doocumentID = documentPath.slice(documentPath.lastIndexOf('/') + 1, documentPath.lastIndexOf('.'));
-    fs.readFile(documentPath, (error, data) => {
-        if (error) {
-            return defer.reject({err : error, at : "Read JSON File"});
-        } else {
-            try {
-                let docBody = data;
-                let options = {
-                    url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName + '/document/' + doocumentID,
-                    method : 'PUT',
-                    body : docBody,
-                }
-                request(options, (err, res, body) => {
-                    if (err) {
-                        return defer.reject({err, at : "Add Document"});
-                    } else {
-                        return defer.resolve({body, status : res.statusCode});
-                    }
-                });
-            } catch (err) {
-                return defer.reject({err, at : "Parse JSON File"});
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
             }
         }
     });
     return defer.promise;
 }
 
-//Delete a document [DELETE] Deletes a document
-let deleteDocument = (params, cb) => {
+//Delete an index [DELETE] Deletes an Index
+let deleteIndex = (params) => {
     let defer = q.defer();
     let indexName = params.indexName;
-    let doocumentID = params.doocumentID;
     let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName + '/document/' + doocumentID,
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}`,
+        method : 'DELETE'
+    }
+    request(options, (err, res, body) => {
+        if (err) {
+            return defer.reject({err, at : "Delete Index"});
+        } else {
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
+            }
+        }
+    });
+    return defer.promise;
+}
+
+//Get all indices [GET] Gets all Indices
+let getAllIndices = () => {
+    let defer = q.defer();
+    let options = {
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}`,
+        method : 'GET'
+    }
+    request(options, (err, res, body) => {
+        if (err) {
+            return defer.reject({err, at : "Get Indices"});
+        } else {
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
+            }
+        }
+    });
+    return defer.promise;
+}
+
+//Add a document [PUT] Adds a document to an index
+let addDocument = (params) => {
+    let defer = q.defer();
+    let indexName = params.indexName;
+    let documentPath = params.documentPath;
+    let documentID = documentPath.slice(documentPath.lastIndexOf('/') + 1, documentPath.lastIndexOf('.'));
+    fs.readFile(documentPath, (error, data) => {
+        if (error) {
+            return defer.reject({err : error, at : "Read JSON File"});
+        } else {
+            let docBody = data;
+            let options = {
+                url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}/${DOCUMENT_URL_EXT}/${documentID}`,
+                method : 'PUT',
+                body : docBody,
+            }
+            request(options, (err, res, body) => {
+                if (err) {
+                    return defer.reject({err, at : "Add Document"});
+                } else {
+                    let statusCode = res.statusCode.toString();
+                    if (statusCode.search(/^[2]\d\d$/) === -1) {
+                        return defer.reject({err : body, success : false});
+                    } else {
+                        return defer.resolve({body, success : true});
+                    }
+                }
+            });
+        }
+    });
+    return defer.promise;
+}
+
+//Delete a document [DELETE] Deletes a document
+let deleteDocument = (params) => {
+    let defer = q.defer();
+    let indexName = params.indexName;
+    let documentID = params.documentID;
+    let options = {
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}/${DOCUMENT_URL_EXT}/${documentID}`,
         method : 'DELETE',
     }
     request(options, (err, res, body) => {
         if (err) {
             return defer.reject({err, at : "Delete Document"});
         } else {
-            return defer.resolve({body, status : res.statusCode});
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
+            }
         }
     });
     return defer.promise;
 }
 
 //Get a document based on name [GET] Searches for a document based on its name
-let getDocument = (params, cb) => {
+let getDocument = (params) => {
     let defer = q.defer();
     let indexName = params.indexName;
-    let doocumentID = params.doocumentID;
+    let documentID = params.documentID;
     let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName + '/document/' + doocumentID,
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}/${DOCUMENT_URL_EXT}/${documentID}`,
         method : 'GET',
     }
     request(options, (err, res, body) => {
         if (err) {
             return defer.reject({err, at : "Get Document"});
         } else {
-            return defer.resolve({body, status : res.statusCode});
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
+            }
         }
     });
     return defer.promise;
 }
 
 //Get number of documents in an index [GET]
-let count = (params, cb) => {
+let count = (params) => {
     let defer = q.defer();
     let indexName = params.indexName;
     let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName + '/_count',
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}/_count`,
         method : 'GET',
     }
     request(options, (err, res, body) => {
         if (err) {
             return defer.reject({err, at : "Get Count of Documents"});
         } else {
-            return defer.resolve({body, status : res.statusCode});
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
+            }
         }
     });
     return defer.promise;
 }
 
 //Search in a format compatible to ElasticSearch [GET]
-let search = (params, cb) => {
+let search = (params) => {
     let defer = q.defer();
     let indexName = params.indexName;
     let searchString = params.searchString;
     let options = {
-        url : `${SEARCH_SERVER}/api/search/v2/index/` + indexName + '/_count',
-        method : 'GET',
-        params : JSON.stringify({"query" : searchString}),
+        url : `${SEARCH_SERVER}/${INDEX_BASE_URL}/${indexName}/_search`,
+        method : 'POST',
+        body : JSON.stringify({"query" : {"query" : searchString}}),
     }
     request(options, (err, res, body) => {
         if (err) {
             return defer.reject({err, at : "Search for Document"});
         } else {
-            return defer.resolve({body, status : res.statusCode});
+            let statusCode = res.statusCode.toString();
+            if (statusCode.search(/^[2]\d\d$/) === -1) {
+                return defer.reject({err : body, success : false});
+            } else {
+                return defer.resolve({body, success : true});
+            }
         }
     });
     return defer.promise;
 }
 
-/*let runManualTests = () => {
+/*
+let runManualTests = () => {
     init().then(value => {
         console.log("Ran test for init");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return createIndex({indexName : "test1"});
-    })
-    .then(value => {
+    }).then(value => {
         console.log("Ran test for index creation and got response");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return addDocument({indexName : "test1", documentPath : "/opt/testDocument.json"});
     }).then(value => {
         console.log("Ran test for document addition and got response ");
-        console.log(value.body + ' ' + value.status);
-        return getDocument({indexName : "test1", doocumentID : "testDocument"});
+        console.log(value.body + ' ' + value.success);
+        return getDocument({indexName : "test1", documentID : "testDocument"});
     }).then(value => {
         console.log("Ran test for document retrieval and got response ");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return count({indexName : "test1"});
     }).then(value => {
         console.log("Ran test for index count and got response ");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return search({indexName : "test1", searchString : "testing"});
     }).then(value => {
         console.log("Ran test for search and got response");
-        console.log(value.body + ' ' + value.status);
-        return deleteDocument({indexName : "test1", doocumentID : "testDocument"});
+        console.log(value.body + ' ' + value.success);
+        return deleteDocument({indexName : "test1", documentID : "testDocument"});
     }).then(value => {
         console.log("Ran test for document deletion and got response");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return deleteIndex({indexName : "test1"});
     }).then(value => {
         console.log("Ran test for index deletion and got response");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
+        console.log("---------------------------------");
+        console.log("Future responses should be errors");
         return addDocument({indexName : "test1", documentPath : "/opt/testDocument.json"});
     }).then(value => {
         console.log("Ran test for document addition when not present and got response ");
-        console.log(value.body + ' ' + value.status);
-        return getDocument({indexName : "test1", doocumentID : "testDocument"});
+        console.log(value.body + ' ' + value.success);
+        return getDocument({indexName : "test1", documentID : "testDocument"});
     }).then(value => {
         console.log("Ran test for document retrieval when not present and got response ");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return count({indexName : "test1"});
     }).then(value => {
         console.log("Ran test for index count when not present and got response ");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return search({indexName : "test1", searchString : "testing"});
     }).then(value => {
         console.log("Ran test for search when not present and got response");
-        console.log(value.body + ' ' + value.status);
-        return deleteDocument({indexName : "test1", doocumentID : "testDocument"});
+        console.log(value.body + ' ' + value.success);
+        return deleteDocument({indexName : "test1", documentID : "testDocument"});
     }).then(value => {
         console.log("Ran test for document deletion when not present and got response");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
         return deleteIndex({indexName : "test1"});
     }).then(value => {
         console.log("Ran test for index deletion when not present and got response");
-        console.log(value.body + ' ' + value.status);
+        console.log(value.body + ' ' + value.success);
     }).catch(e => {
-        console.log("Got error at: " + e.at);
+        console.log("Got error");
         console.log("Error message: " + e.err);
-        console.log("Error: " + e);
     });
 }
 
-runManualTests();*/
+runManualTests();
+*/
 
 module.exports = {
-    init, createIndex, addDocument, deleteIndex, deleteDocument, getDocument, count, search, getAllIndices,
+    init,
+    createIndex,
+    addDocument,
+    deleteIndex,
+    deleteDocument,
+    getDocument,
+    count,
+    search,
+    getAllIndices
 }
