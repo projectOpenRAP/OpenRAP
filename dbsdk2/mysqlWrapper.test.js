d7c820219efd492cdc815f398f22c074ada8e7fe
rak16 <ravishahmad16@gmail.com>
2018-02-28 12:05:01 +0530

    Add more DML functions, rename `index.js` to `mysqlWrapper.js` and `test.js` to `mysqlWrapper.test.js`

---
+++ dbsdk2/mysqlWrapper.test.js
@@ -0,0 +1,247 @@
+'use strict'
+
+// const db = require('./index.js');
+
+// db.createDatabase({db : { name : 'test'} })
+//     .then(
+//         (response) => {
+//             console.log("Execution status : " + response.success);
+//             console.log("Message : " + response.message);
+//         },
+//         (error) => {
+//             console.log("Execution status : " + error.success);
+//             console.log("Error : \n" + error.message);
+//         }
+//     );
+
+
+// db.listDatabases()
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// db.deleteDatabase({ db : { name : 'test' } })
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//         }
+//             console.log(error);
+//     );
+
+
+// let tableDef = {
+//     db : {
+//         name : 'test',
+//     },
+//
+//     table : {
+//         name : 'Orders',
+//         fields : [
+//             {
+//                 name : 'OrderID',
+//                 type : 'int',
+//                 isNullable : false,
+//                 isPrimary : true,
+//                 // references : {
+//                 //     table : 'Person',
+//                 //     field : 'ParentKey'
+//                 // }
+//             },
+//
+//             {
+//                 name : 'OrderNumber',
+//                 type : 'int',
+//                 isNullable : true,
+//                 isPrimary : false,
+//             },
+//         ]
+//     }
+// }
+//
+// db.createTable(tableDef)
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// db.listTables({db : { name : 'test' }})
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// db.deleteTable({db : { name : 'test' }, table : { name : 'Orders2' }})
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// let toAdd = {
+//     db : {
+//         name : 'test',
+//     },
+//
+//     table : {
+//         name : 'Orders',
+//         fields : ['OrderNumber', 'OrderID'],
+//         values : ['107','44']
+//     }
+// }
+//
+// db.addRecord(toAdd)
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// let toRead = {
+//     "db" : {
+//         "name" : "test",
+//     },
+//
+//     "table" : {
+//         "name" : "Orders",
+//         "fields" : ["OrderNumber", "OrderID"],
+//         "filters" : [
+//             {
+//                 "by" : "OrderNumber",
+//                 "onlyIf" : "IN (1, 2)"
+//             },
+//             {
+//                 "by" : "OrderID",
+//                 "if" : "< 12"
+//             }
+//         ],
+//         "sort" : [
+//             {
+//                 "by" : "OrderID",
+//                 "desc" : true
+//             },
+//             {
+//                 "by" : "OrderNumber",
+//                 "desc" : false
+//             }
+//         ]
+//     }
+// }
+//
+// db.readRecords(toRead)
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// let toUpdate = {
+//     "db" : {
+//         "name" : "test",
+//     },
+//
+//     "table" : {
+//         "name" : "Orders",
+//         "fields" : [
+//             {
+//                 "name" : "OrderNumber",
+//                 "value" : "999"
+//             }
+//         ],
+//         "filters" : [
+//             {
+//                 "by" : "OrderID",
+//                 "if" : "",
+//                 "onlyIf" : "= 12"
+//             },
+//             {
+//                 "by" : "OrderNumber",
+//                 "if" : "",
+//                 "onlyIf" : "= 1"
+//             }
+//         ]
+//     }
+// }
+//
+// db.updateRecords(toUpdate)
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// let toDelete = {
+//     "db" : {
+//         "name" : "test",
+//     },
+//
+//     "table" : {
+//         "name" : "Orders",
+//         "filters" : [
+//             {
+//                 "by" : "OrderNumber",
+//                 "if" : "= 107",
+//                 "onlyIf" : ""
+//             },
+//             {
+//                 "by" : "OrderID",
+//                 "if" : "",
+//                 "onlyIf" : "= 40"
+//             }
+//         ]
+//     }
+// }
+//
+// db.deleteRecord(toDelete)
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
+
+
+// db.clearRecords({db : { name : 'test' }, table : { name : 'Orders' }})
+//     .then(
+//         (response) => {
+//             console.log(response);
+//         },
+//         (error) => {
+//             console.log(error);
+//         }
+//     );
