f31da79e06f325cebb75f821f8158035b8f83993
SumukhaKV <kvsumukha@gmail.com>
2018-05-28 15:30:51 +0530

    Make sidebar fit the screen height

--- devmgmtui/src/components/common/Sidebar.js
+++ devmgmtui/src/components/common/Sidebar.js
@@ -29,47 +29,49 @@ class SideNav extends Component {
 
         if (typeof this.props.auth.user !== `undefined`) {
         return (
-            <Sidebar.Pushable style={{ height: '100%' }}>
-                <Sidebar as={Menu} animation='scale down' width='thin' visible={this.state.sideBarVisible} icon='labeled' vertical inverted>
-                    {this.props.auth.user.permissions.search(/VIEW_DASHBOARD|ALL/) >= 0 ? <Menu.Item name='home' active={this.state.currentLocation === '/dashboard'} as={Link} to={'/dashboard'}>
-                        <Icon name='home' />
-                        Home
-                    </Menu.Item> : null }
-                    {this.props.auth.user.permissions.search(/VIEW_USERS|ALL/) >= 0 ? <Menu.Item name='users' active={this.state.currentLocation === '/users'} as={Link} name='users' to={'/users'}>
-                        <Icon name='users' />
-                        Users
-                    </Menu.Item> : null }
-                    {this.props.auth.user.permissions.search(/UPGRADE_DEVICE|ALL/) >= 0 ? <Menu.Item name='upgrade' active={this.state.currentLocation === '/upgrade'} as={Link} to={'/upgrade'}>
-                      <Icon name='up arrow' />
-                      Upgrade
-                    </Menu.Item> : null}
-                    { this.props.auth.user.permissions.search(/VIEW_FILES|ALL/) >= 0 ? <Menu.Item name='filemgmt' active={this.state.currentLocation === '/filemgmt'} as={Link} to={'/filemgmt'}>
-                      <Icon name='disk outline' />
-                      File Management
-                    </Menu.Item> : null }
-                    { this.props.auth.user.permissions.search(/MODIFY_SSID|ALL/) >= 0 ? <Menu.Item name='modify_ssid' active={this.state.currentLocation === '/ssid/set'} as={Link} to={'/ssid/set'}>
-                      <Icon name='wifi' />
-                      Modify SSID
-                    </Menu.Item> : null }
-                    { this.props.auth.user.permissions.search(/CHANGE_CAPTIVE_PORTAL|ALL/) >= 0 ? <Menu.Item name='captive_mod' active={this.state.currentLocation === '/captive'} as={Link} to={'/captive'}>
-                      <Icon name='eye' />
-                      Modify Captive Portal
-                    </Menu.Item> : null }
+            <div style={{ height: '100vh' }}>
+                <Sidebar.Pushable>
+                    <Sidebar as={Menu} animation='scale down' width='thin' visible={this.state.sideBarVisible} icon='labeled' vertical inverted>
+                        {this.props.auth.user.permissions.search(/VIEW_DASHBOARD|ALL/) >= 0 ? <Menu.Item name='home' active={this.state.currentLocation === '/dashboard'} as={Link} to={'/dashboard'}>
+                            <Icon name='home' />
+                            Home
+                        </Menu.Item> : null }
+                        {this.props.auth.user.permissions.search(/VIEW_USERS|ALL/) >= 0 ? <Menu.Item name='users' active={this.state.currentLocation === '/users'} as={Link} name='users' to={'/users'}>
+                            <Icon name='users' />
+                            Users
+                        </Menu.Item> : null }
+                        {this.props.auth.user.permissions.search(/UPGRADE_DEVICE|ALL/) >= 0 ? <Menu.Item name='upgrade' active={this.state.currentLocation === '/upgrade'} as={Link} to={'/upgrade'}>
+                          <Icon name='up arrow' />
+                          Upgrade
+                        </Menu.Item> : null}
+                        { this.props.auth.user.permissions.search(/VIEW_FILES|ALL/) >= 0 ? <Menu.Item name='filemgmt' active={this.state.currentLocation === '/filemgmt'} as={Link} to={'/filemgmt'}>
+                          <Icon name='disk outline' />
+                          File Management
+                        </Menu.Item> : null }
+                        { this.props.auth.user.permissions.search(/MODIFY_SSID|ALL/) >= 0 ? <Menu.Item name='modify_ssid' active={this.state.currentLocation === '/ssid/set'} as={Link} to={'/ssid/set'}>
+                          <Icon name='wifi' />
+                          Modify SSID
+                        </Menu.Item> : null }
+                        { this.props.auth.user.permissions.search(/CHANGE_CAPTIVE_PORTAL|ALL/) >= 0 ? <Menu.Item name='captive_mod' active={this.state.currentLocation === '/captive'} as={Link} to={'/captive'}>
+                          <Icon name='eye' />
+                          Modify Captive Portal
+                        </Menu.Item> : null }
 
-                    <Menu.Item name='logout' as={Link} to={'/'} onClick={(dispatch) => dispatch({ type: "DISABLE_AUTH"})}>
-                        <Icon name='log out' />
-                        Logout
-                    </Menu.Item>
-                </Sidebar>
-                <Sidebar.Pusher>
-                    <Menu fixed='top' inverted>
-                        <Menu.Item name='bars' onClick={this.toggleSideBarVisibility.bind(this)}>
-                            <Icon name='bars' />
+                        <Menu.Item name='logout' as={Link} to={'/'} onClick={(dispatch) => dispatch({ type: "DISABLE_AUTH"})}>
+                            <Icon name='log out' />
+                            Logout
                         </Menu.Item>
-                    </Menu>
-                    {this.props.children}
-                </Sidebar.Pusher>
-            </Sidebar.Pushable>
+                    </Sidebar>
+                    <Sidebar.Pusher>
+                        <Menu fixed='top' inverted>
+                            <Menu.Item name='bars' onClick={this.toggleSideBarVisibility.bind(this)}>
+                                <Icon name='bars' />
+                            </Menu.Item>
+                        </Menu>
+                        {this.props.children}
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </div>
         );
       } else {
         this.props.history.push("/");
