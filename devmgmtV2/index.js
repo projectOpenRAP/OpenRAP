6fcb8734aa58abf53a4439f396edb84812280e28
Skeletrox <android.svr@gmail.com>
2018-06-11 14:20:04 +0530

    create CRON jobs for internet and users

--- devmgmtV2/index.js
+++ devmgmtV2/index.js
@@ -1,10 +1,116 @@
 "use strict"
 
+let internetConnectivitySkeleton = {
+        edata: {
+            type: "system", // Required. Type of log (system, process, api_access, api_call, job, app_update etc)
+            level: "INFO", // Required. Level of the log. TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+            message: "Established internet connectivity", // Required. Log message
+            params: [
+                {
+                    timestamp: ""
+                }
+            ] // Optional. Additional params in the log message
+        }
+    }
+
+let usersConnectedSkeleton = {
+    edata: {
+        level: "INFO",
+        message: "Number of users fetched",
+        params: [
+            {
+                count: 5,
+                timestamp: "2018-05-20 03:01:07"
+            }
+        ],
+        type: "api_call"
+    }
+}
+
 let express = require("express");
 let bodyParser = require('body-parser');
 let cors = require('cors');
 let app = express();
-let { exec } = require('child_process')
+let { exec } = require('child_process');
+let path = require('path');
+
+let internetConnected = null;
+let numberOfUsers = null;
+let timeInterval = 15000;
+
+let generateTelemetry = (telemetryType, telemetryValue) => {
+    let telemetryNow = null;
+    switch(telemetryType) {
+        case 'internetConnected' :
+            telemetryNow = internetConnectivitySkeleton;
+            telemetryNow.edata.message = telemetryValue ? "Established Internet connectivity" : "Lost Internet connectivity";
+            break;
+        case 'usersConnected' :
+            telemetryNow = usersConnectedSkeleton;
+            telemetryNow.edata.params[0].count = parseInt(telemetryValue);
+            break;
+        default :
+            break;
+        if (telemetryNow !== null) {
+            let now = new Date();
+            let nowAsString = now.getFullYear() + '-' + now.getMonth() + '-' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+            telemetryNow.edata.params[0].timestamp = nowAsString;
+
+        }
+    }
+}
+
+let isInternetActive = () => {
+    /*
+        DUMMY FUNCTION
+        TODO: Replace with import from telemetrySDK
+    */
+    let defer = q.defer();
+    defer.resolve();
+    return defer.promise;
+}
+
+let repeatedlyCheckForInternet = () => {
+    let internetChecker = () => {
+        isInternetActive().then(value => {
+            let currentConnection = true;
+            if (currentConnection !== internetConnected) {
+                internetConnected = currentConnection;
+                generateTelemetry('internetConnected', currentConnection);
+            }
+        }).catch(e => {
+            let currentConnection = false;
+            if (currentConnection !== internetConnected) {
+                internetConnected = currentConnection;
+                generateTelemetry('internetConnected', currentConnection);
+            }
+        });
+    }
+    setInterval(internetChecker, timeInterval);
+}
+
+let repeatedlyCheckUsers = () => {
+    const cmd = path.join(__dirname, '../../CDN/getall_stations.sh');
+    let userChecker = () => exec(cmd, { shell: '/bin/bash' }, (err, stdout, stderr) => {
+        // console.log('Error: ' + err);
+        // console.log('stdout: ' + stdout);
+        // console.log('stderr: ' + stderr);
+
+        if (err !== null) {
+            let usersConnected = stdout.trim();
+            if (usersConnected !== numberOfUsers) {
+                numberOfUsers = usersConnected;
+                generateTelemetry('usersConnected', usersConnected);
+            }
+        }
+    });
+    setInterval(userChecker, timeInterval);
+}
+
+let runCronBgServices = () => {
+    repeatedlyCheckForInternet();
+    repeatedlyCheckUsers();
+}
 
 app.use(cors())
 // parse application/x-www-form-urlencoded
