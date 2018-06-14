2e9c534e789ccce06ad0b7711f373615eaf1aa1b
SumukhaKV <kvsumukha@gmail.com>
2018-06-14 14:52:13 +0530

    Pass necessary input in accordance with dbsdk2's JSON structure

--- devmgmtV2/writeToDB.js
+++ devmgmtV2/writeToDB.js
@@ -1,7 +1,18 @@
-let {selectFields, updateFields, deleteFields, insertFields} = require('dbsdk');
-// console.log(dbsdk);
+let {addRecord} = require('dbsdk2');
 
-insertFields({dbName : 'device_mgmt', tableName : 'users', columns : ["username", "password", "permission"], values : ["root", "root", JSON.stringify(["ALL"])]}).then(response => {
+addRecord(
+    {
+        "db" : {
+            "name" : "device_mgmt",
+        },
+
+        "table" : {
+            "name" : "users",
+            "fields" : ["username", "password", "permission"],
+            "values" : ["root", "root", JSON.stringify(["ALL"])]
+        }
+    }
+).then(response => {
   console.log("Initialized Database");
 }, reject => {
   console.log("Using already built configuration");
