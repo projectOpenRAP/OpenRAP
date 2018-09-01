107333c3c00a97e2a118a0ec0fff862e87225a15
rak16 <ravishahmad16@gmail.com>
2018-09-01 17:08:50 +0530

    Fix xcontent path

--- appServer/plugins/ekStep/ekstep.controller.js
+++ appServer/plugins/ekStep/ekstep.controller.js
@@ -788,6 +788,7 @@ let moveInternalFolders = (dir, fileNameAsFolder) => {
                         console.log("767");
                     } else if (stats.isDirectory()) {
                         console.log("directory found");
+                        internalFolder = files[i];
                         moveFilePromises.push(moveFileWithPromise(folder + internalFolder, dir + 'xcontent/' + internalFolder));
                         return defer.resolve(files[i]);
                     }
