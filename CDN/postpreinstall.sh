aec3a42068505f7b4349a07a66ce4a2fb55da2e8
rak16 <ravishahmad16@gmail.com>
2019-02-18 22:55:23 +0530

    Do not restart devmgmt before reboot
    (cherry picked from commit 796b924a7bd05405d2c8916bcf507684edb039be)

--- CDN/postpreinstall.sh
+++ CDN/postpreinstall.sh
@@ -12,7 +12,7 @@ for i in $files_saved
 do
 	 cp $i /tmp
 done
-	
+
 }
 
 restore_files()
@@ -62,9 +62,6 @@ systemctl restart searchserver
 systemctl enable appserver
 systemctl restart appserver
 
-systemctl enable devmgmt
-systemctl restart devmgmt
-
 systemctl enable syncthing
 systemctl restart syncthing
 
@@ -73,8 +70,10 @@ systemctl restart aria2
 
 reboot_device
 
-exit 0
+systemctl enable devmgmt
+systemctl restart devmgmt
 
+exit 0
 }
 
 pre_install()
@@ -90,7 +89,7 @@ if [ "$1" == "-post" ]
 	then
 	post_install
 fi
-	
+
 if [ "$1" == "-pre" ]
 		then
 		pre_install
