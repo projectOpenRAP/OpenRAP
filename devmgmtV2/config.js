b5369eedbc4eac926d9eb02a6854fe726e127d77
rak16 <ravishahmad16@gmail.com>
2019-03-23 17:39:05 +0530

    Update devmgmtV2 config

--- devmgmtV2/config.js
+++ devmgmtV2/config.js
@@ -1,8 +1,13 @@
+/*
+ * This file is only a placeholder and will be replaced by the config.js under the plugin directory.
+ */
+
 module.exports = {
 	"FS_ROOT": "/home/admin/",
 	"username": "brandonStark",
 	"password": "theBuilder",
 	"serverIP": "35.240.131.174",
+    "root_dir": "/home/admin/diksha/",
 	"telemetryAPI": {
 		"init": "http://35.240.131.174:8888/api/auth/v1/init", // Authenticates user and returns a token
 		"hello": "http://35.240.131.174:8000/api/auth/v1/hello", // Checks if the user is authenticated
@@ -16,13 +21,13 @@ module.exports = {
 			"user_token": "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ1WXhXdE4tZzRfMld5MG5PS1ZoaE5hU0gtM2lSSjdXU25ibFlwVVU0TFRrIn0.eyJqdGkiOiJjMDc0ODgxYi01OGYxLTQ2YzAtYWUwZC1hNTkwZGVkZWRhMTUiLCJleHAiOjE1MzYzNDc0MzcsIm5iZiI6MCwiaWF0IjoxNTM2MzA0MjM3LCJpc3MiOiJodHRwczovL3N0YWdpbmcub3Blbi1zdW5iaXJkLm9yZy9hdXRoL3JlYWxtcy9zdW5iaXJkIiwiYXVkIjoiYWRtaW4tY2xpIiwic3ViIjoiNWJkODIwYTItNTk5Zi00ZDEyLTliYzQtZjg3YTg5MjU2ZGU2IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWRtaW4tY2xpIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiYTZlNjZjOTMtNjliYS00NmM3LWJkMzAtMDE2MzAxOGQ3ZGI5IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6W10sInJlc291cmNlX2FjY2VzcyI6e30sIm5hbWUiOiLgrobgrprgrr_grrDgrr_grq_grrDgr40iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjb250ZW50Y3JlYXRvcmp1bmVvcmdAc3VuYmlyZC1zdGFnaW5nIiwiZ2l2ZW5fbmFtZSI6IuCuhuCumuCuv-CusOCuv-Cur-CusOCvjSIsImZhbWlseV9uYW1lIjoiIiwiZW1haWwiOiJjb250ZW50Y3JlYXRvckBqdW5lb3JnLmNvbSJ9.RIDIsVXvTY5UPibUYxaQKtS5ARhwPFPDYsJw_n2Xe5u_xJuxkTCCceO8ECojnt-9WRrDhUwQm7kiAK0h-4LFcqOrDwsXmh09xvAQ_FNcTbeRL0Rg_Qj1rQgNHE9M7tmZx1LZUe8FzDwxFhaQaeQ-wb9-o4wuAGjt5AuNQy6ffmS29iZlpuxLoJGy89b61Cytm-h_GAGfG9qmqIPQ_HIfT0XiEyuuHXS-A4Kiii5qDusTlb78mJRGbQ_BOBlRNmmCpVBUVyJ-AwjJyg7RHaHz9uHn0iRvh-BHH9xSfpxLSZxlkLCilJ048uLqjEwMGc35RqIspazW19cAa4XPwEO4Mg"
 		},
 		"app_register_api": {
-			"url": "",
-			"token": ""
-		},
-		"device_register_api": {
-			"url": "",
-			"token": ""
-		},
+            "url": "https://diksha.gov.in/api/api-manager/v1/consumer/mobile_app_openrap/credential/register",
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZjg5NGZhOTAzY2M0NWU3ODc2NzdkNTdhMmJjZDY1NyJ9.j6PENpl5brIYYRoSf7kHI9JCHxQit-4_eKXJkBR_OVM"
+        },
+        "device_register_api": {
+            "url": "https://diksha.gov.in/api/api-manager/v1/consumer/mobile_device_openrap/credential/register",
+            "token": ""
+        },
 		"filter": {
 			"keysToUse": [
 				"subject",
