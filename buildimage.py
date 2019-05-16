fac96e1332891f7404e2f6f0edf5a25cc8208764
Shree05Ram <shreeram@aikaan.io>
2019-05-16 11:46:05 +0530

    Bug: Even when the forwater build was created, Diksha app was getting downloaded from the captive portal. RCA: The variant was not applicable for downloading the app. FIx: Both Diksha and forWater apk files have been copied into rootfs_overlay/var/www/html/public folder. Depending on the variant corresponding app gets downloaded.

--- buildimage.py
+++ buildimage.py
@@ -457,6 +457,14 @@ class Profile(Device):
         #cmd = "cp -rf profile/" + self.profiletype + "/rootfs_overlay" + " " + self.imgdir
         #run_cmd(cmd)
 
+        #Rename variant.apk as app.apk
+        cmd = "cp rootfs_overlay/var/www/html/public/" + variant + ".apk" + " " + "rootfs_overlay/var/www/html/public/app.apk"
+        run_cmd(cmd)
+
+        # Copy rootfs_overlay 
+        cmd = "cp -rf rootfs_overlay" + " " + self.imgdir
+        run_cmd(cmd)
+
         # Update /etc/hostname file
         filename = self.imgdir + "rootfs_overlay/etc/hostname"
         file_hostname_update(filename, self.hostname)
